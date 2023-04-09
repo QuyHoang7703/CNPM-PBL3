ad3a452492795ef9fc8f4fd4584c41edc328b485
QuyHoang7703 <hoangvanquy772003@gmail.com>
2023-04-09 11:44:08 +0700

    Login

--- View/Form1.cs
+++ View/Form1.cs
@@ -17,7 +17,7 @@ namespace CNPM_PBL3
             InitializeComponent();
         }
 
-        private void guna2Button_quenMK_Click(object sender, EventArgs e)
+        private void guna2Button_ForgotPassword_Click(object sender, EventArgs e)
         {
             panel_login.Visible = false;
         }
@@ -27,20 +27,20 @@ namespace CNPM_PBL3
             panel_login.Visible = true;
         }
 
-        private void picture_hien_Click(object sender, EventArgs e)
+        private void picture_Invisible_Click(object sender, EventArgs e)
         {
             if (guna2TextBox_pasword.PasswordChar == '\0')
             {
-                picture_an.BringToFront();
+                picture_Visible.BringToFront();
                 guna2TextBox_pasword.PasswordChar = '•';
             }
         }
 
-        private void picture_an_Click_1(object sender, EventArgs e)
+        private void picture_Visible_Click_1(object sender, EventArgs e)
         {
             if (guna2TextBox_pasword.PasswordChar == '•')
             {
-                picture_hien.BringToFront();
+                picture_Invisible.BringToFront();
                 guna2TextBox_pasword.PasswordChar = '\0';
             }
         }
@@ -51,6 +51,11 @@ namespace CNPM_PBL3
         }
 
         private void guna2CircleButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
         {
 
         }
